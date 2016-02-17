4a8f6901970f65ee958c2cb10f86c374144a0890
unknown <jcarrillo@DST-L079.DSTControls.local>
2016-02-17 15:36:19 -0800

    Assigment 2 completed

--- RestaurantManager/RestaurantManager/OrderPage.xaml.cs
+++ RestaurantManager/RestaurantManager/OrderPage.xaml.cs
@@ -13,29 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace RestaurantManager
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class OrderPage : Page
     {
-        public MainPage()
+        public OrderPage()
         {
             this.InitializeComponent();
         }
 
-        private void OrderPage_Click(object sender, RoutedEventArgs e)
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
-            
-            this.Frame.Navigate(typeof(OrderPage));    
-        }
-
-        private void ExpeditePage_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(ExpeditePage));
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
